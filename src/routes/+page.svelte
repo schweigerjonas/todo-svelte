f239025c9f26b44b0b88c149752bf6569d7c83ab
Jonas Schweiger <jonas.schweiger@outlook.de>
2025-11-11 10:51:54 +0100

    Add bulma and simple landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+</script>
+
+<div class="container">
+	<div class="box has-centered-text">
+		<div class="title">Simple Todo List</div>
+		<div class="subtitle">Fun things to do</div>
+	</div>
+</div>
